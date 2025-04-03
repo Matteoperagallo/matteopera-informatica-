d8f8571650169404a3aab59d11ec28a7acdf31b6
Matteoperagallo <matteoperagallo@uca.edu.ar>
2025-04-03 19:39:46 -0300

    Add files via upload

---
+++ ejercicios informatica 6.py
@@ -0,0 +1,27 @@
+
+frutas = ("manzana","pera","uva","mandarina")
+frutas.apenned("banana")
+frutas.insert(2,"durazno")
+frutas.sort()
+frutas.reverse()
+for fruta in frutas:  
+    print(fruta)
+    
+
+
+'''
+
+#while #repite un bloque mientras una condicion se verdadera
+#contador = 5 // asigno un valor a la variable 
+#contador == 5 // hacer una comparacion 
+
+
+
+
+
+contador=0
+while contador < 5:
+    print(contador)
+    contador +=1
+   
+    
