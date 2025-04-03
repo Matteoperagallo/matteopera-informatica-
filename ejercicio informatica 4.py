9934b4d1087b5a2546dab22922c91446a7aa9ac3
Matteoperagallo <matteoperagallo@uca.edu.ar>
2025-04-03 18:43:46 -0300

    Add files via upload

---
+++ ejercicio informatica 4.py
@@ -0,0 +1,10 @@
+clasificacion =10
+
+if clasificacion >= 90:
+    print("excelente")
+elif clasificacion >= 80:
+    print("muy bueno")
+elif clasificacion >= 70:
+    print("bueno")
+elif clasificacion < 70:
+    print("necesita mejorar")
