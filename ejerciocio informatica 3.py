c21d281223f2c81c44a9fd1d6066d45c8cba6757
Matteoperagallo <matteoperagallo@uca.edu.ar>
2025-04-03 18:43:08 -0300

    Add files via upload

---
+++ ejerciocio informatica 3.py
@@ -0,0 +1,9 @@
+numero1 = float(input("ingresa el primer numero:")
+numero2 = float(input("ingresa el segundo numnero:")
+
+if numero1 > numero2:
+    print("el primer numero es mayor que el segundo")
+if numero2 > numero1:
+    print("el segundo numero es mayor que el primero")
+else:
+    print("ambos numeros son iguales")
